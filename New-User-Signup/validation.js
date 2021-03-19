2e9c377ee42487bce98543e3b32f9394ef95886b
enPlace <n.place@protonmail.com>
2021-03-19 14:13:17 -0300

    initial commit

---
+++ New-User-Signup/validation.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll('input');
+const patterns ={
+    telephone: /^\d{11}$/
+}; 
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup', (e)=>{
+         console.log(e.target.attributes.name.value);
+    })
+})
