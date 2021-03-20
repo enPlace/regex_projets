8bd0c5e9e46e7e7ecc4e5af3d9a92fb412c7bd38
enPlace <n.place@protonmail.com>
2021-03-20 14:10:36 -0300

    validation function

--- New-User-Signup/validation.js
+++ New-User-Signup/validation.js
@@ -6,13 +6,17 @@ const patterns ={
 
 //validateion function 
 function validate(field, regex){
-	console.log(regex.test(field.value));
+	if(regex.test(field.value)){
+        field.className = 'valid'
+    }else{
+        field.className = 'invalid'
+    }
 }
 
 inputs.forEach((input)=>{
     input.addEventListener('keyup', (e)=>{
          //console.log(e.target.attributes.name.value);
-         validate(e.target, patterns['telephone'])
+         validate(e.target, patterns[e.target.attributes.name.value])
     })
 })
 
